f8e56f5108b592e0f6b9d60e71ce14e3bb0d7714
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2024-11-21 16:22:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -56,7 +56,8 @@ class Solution {
         Node* head =new Node(arr[0]);
         Node* curr=head;
         for(int i=1; i<arr.size(); i++){
-            curr=curr->next=new Node(arr[i]);
+            curr->next=new Node(arr[i]);
+            curr=curr->next;
         }
         return head;
     }
