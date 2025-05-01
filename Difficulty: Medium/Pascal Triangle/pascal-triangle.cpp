a6f250d58d829ba2cbdae68b073970b85cf6f9c4
ANISH KUMAR SINGH <129846065+anishsingh234@users.noreply.github.com>
2025-05-01 22:02:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> nthRowOfPascalTriangle(int n) {
+        // code here
+         vector<int> result;
+         
+         long long ans = 1;
+          result.push_back(ans);
+         for(int i=1; i<n; i++){
+             ans=ans*(n-i);
+             ans=ans/i;
+             result.push_back(ans);
+         }
+         return result;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+void printAns(vector<int> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
